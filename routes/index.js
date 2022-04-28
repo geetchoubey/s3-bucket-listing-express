592304be969caac0df7d6e7b1af19ac7a5ef9f1a
Geet Choubey <geetchoubey@gmail.com>
2022-04-28 13:09:46 -0400

    Initial commit

---
+++ routes/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const router = express.Router();
+const AWS = require('aws-sdk');
+
+const s3 = new AWS.S3();
+
+const BUCKET_NAME = process.env.BUCKET_NAME;
+
+/* GET home page. */
+router.get('/*', async function (req, res, next) {
+  const prefix = req.path !== '/'? req.path.substring(1) : ''
+  const objects = await s3.listObjectsV2({
+    Bucket: BUCKET_NAME,
+    Delimiter: req.path,
+    Prefix: prefix,
+  }).promise();
+  res.render('index', {
+    title: 'Express',
+    isRoot: req.path === '/',
+    path: req.path,
+    bucketName: BUCKET_NAME,
+    directories: objects.CommonPrefixes.map(d => d.Prefix),
+    files: objects.Contents.map(k => k.Key.split(' ').join('+'))
+  });
+});
+
+module.exports = router;
